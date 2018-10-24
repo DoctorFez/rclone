257a7f2a7a06021504958261c0bb3701724347c7
Diana Arrieta <diana.arrieta@collectivehealth.com>
2018-10-24 12:22:18 -0700

    Add more insecure ciphers.

--- backend/sftp/sftp.go
+++ backend/sftp/sftp.go
@@ -322,7 +322,7 @@ func NewFs(name, root string, m configmap.Mapper) (fs.Fs, error) {
 
 	if opt.UseInsecureCipher {
 		sshConfig.Config.SetDefaults()
-		sshConfig.Config.Ciphers = append(sshConfig.Config.Ciphers, "aes128-cbc")
+		sshConfig.Config.Ciphers = append(sshConfig.Config.Ciphers, "aes128-cbc aes256-cbc aes192-cbc 3des-cbc")
 	}
 
 	// Add ssh agent-auth if no password or file specified
